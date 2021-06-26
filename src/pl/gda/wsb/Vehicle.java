52e4cf955fc90ede383bdfb902742f3d94a4e78d
Dawid Reszka <dawid.raul@gmail.com>
2021-06-26 19:43:27 +0200

    Zmieniono zmienną vin_number na vinNumber

--- src/pl/gda/wsb/Vehicle.java
+++ src/pl/gda/wsb/Vehicle.java
@@ -3,17 +3,17 @@ package pl.gda.wsb;
 public abstract class Vehicle {
 
     String registration_numbers;
-    String vin_number;
+    String vinNumber;
     String color;
     double price;
     double carCombustion;
     double fuelTankCondition;
     double kilometersTraveled;
 
-    public Vehicle(String registration_numbers, String vin_number, String color, double price, double carCombustion, double fuelTankCondition, double kilometersTraveled)
+    public Vehicle(String registration_numbers, String vinNumber, String color, double price, double carCombustion, double fuelTankCondition, double kilometersTraveled)
     {
     this.registration_numbers = registration_numbers;
-    this.vin_number= vin_number;
+    this.vinNumber= vinNumber;
     this.color= color;
     this.price= price;
     this.carCombustion= carCombustion;
@@ -22,21 +22,23 @@ public abstract class Vehicle {
     }
 
     public class Car extends Vehicle{
+        double number_of_dors;
 
-        public Car(String registration_numbers, String vin_number, String color, double price, double carCombustion, double fuelTankCondition, double kilometersTraveled){
-            super(registration_numbers, vin_number, color, price, carCombustion, fuelTankCondition, kilometersTraveled);
+
+        public Car(String registration_numbers, String vinNumber, String color, double price, double carCombustion, double fuelTankCondition, double kilometersTraveled){
+            super(registration_numbers, vinNumber, color, price, carCombustion, fuelTankCondition, kilometersTraveled);
         }
     }
     public class Truck extends Vehicle{
 
-        public Truck(String registration_numbers, String vin_number, String color, double price, double carCombustion, double fuelTankCondition, double kilometersTraveled){
-            super(registration_numbers, vin_number, color, price, carCombustion, fuelTankCondition, kilometersTraveled);
+        public Truck(String registration_numbers, String vinNumber, String color, double price, double carCombustion, double fuelTankCondition, double kilometersTraveled){
+            super(registration_numbers, vinNumber, color, price, carCombustion, fuelTankCondition, kilometersTraveled);
         }
     }
     public class Motorcycle extends Vehicle{
 
-        public Motorcycle(String registration_numbers, String vin_number, String color, double price, double carCombustion, double fuelTankCondition, double kilometersTraveled){
-            super(registration_numbers, vin_number, color, price, carCombustion, fuelTankCondition, kilometersTraveled);
+        public Motorcycle(String registration_numbers, String vinNumber, String color, double price, double carCombustion, double fuelTankCondition, double kilometersTraveled){
+            super(registration_numbers, vinNumber, color, price, carCombustion, fuelTankCondition, kilometersTraveled);
         }
     }
 
