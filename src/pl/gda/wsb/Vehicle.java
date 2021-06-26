7febc4bd4102e6dbc6e55eced4fcc5494a25bb80
Dawid Reszka <dawid.raul@gmail.com>
2021-06-26 19:07:35 +0200

    3_zadanie

--- src/pl/gda/wsb/Vehicle.java
+++ src/pl/gda/wsb/Vehicle.java
@@ -2,5 +2,26 @@ package pl.gda.wsb;
 
 public abstract class Vehicle {
 
+    String registration_numbers;
+    String vin_number;
+    String color;
+    double price;
+    double carCombustion;
+    double fuelTankCondition;
+    double kilometersTraveled;
+
+    public Vehicle(String registration_numbers, String vin_number, String color, double price, double carCombustion, double fuelTankCondition, double kilometersTraveled)
+    {
+    this.registration_numbers = registration_numbers;
+    this.vin_number= vin_number;
+    this.color= color;
+    this.price= price;
+    this.carCombustion= carCombustion;
+    this.fuelTankCondition= fuelTankCondition;
+    this.kilometersTraveled= kilometersTraveled;
+    }
+
+
+
 
 }
