03dd77aacf4653ade86e4370d931859d9372ca25
Dawid Reszka <dawid.raul@gmail.com>
2021-06-26 19:52:26 +0200

    5_zadanie

--- src/pl/gda/wsb/Vehicle.java
+++ src/pl/gda/wsb/Vehicle.java
@@ -22,22 +22,28 @@ public abstract class Vehicle {
     }
 
     public class Car extends Vehicle{
+        double numberOfDors;
 
 
         public Car(String registration_numbers, String vinNumber, String color, double price, double carCombustion, double fuelTankCondition, double kilometersTraveled){
             super(registration_numbers, vinNumber, color, price, carCombustion, fuelTankCondition, kilometersTraveled);
+            this.numberOfDors = numberOfDors;
         }
     }
     public class Truck extends Vehicle{
+        double capacity;
 
         public Truck(String registration_numbers, String vinNumber, String color, double price, double carCombustion, double fuelTankCondition, double kilometersTraveled){
             super(registration_numbers, vinNumber, color, price, carCombustion, fuelTankCondition, kilometersTraveled);
+            this.capacity = capacity;
         }
     }
     public class Motorcycle extends Vehicle{
+        double horsepower;
 
         public Motorcycle(String registration_numbers, String vinNumber, String color, double price, double carCombustion, double fuelTankCondition, double kilometersTraveled){
             super(registration_numbers, vinNumber, color, price, carCombustion, fuelTankCondition, kilometersTraveled);
+            this.horsepower = horsepower;
         }
     }
 
